63bed7582cee254fb6bb70eb5e8d6d7eff89d6d5
Robert Grassian <rgrassian@berkeley.edu>
2018-11-03 10:10:37 -0700

    Created option to remove face from list when recognized

--- face.py
+++ face.py
@@ -16,18 +16,19 @@ class System:
         params = {
             'returnFaceId': 'true',
             'returnFaceLandmarks': 'false',
-            'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,' +
-            'emotion,hair,makeup,occlusion,accessories,blur,exposure,noise'
+            'returnFaceAttributes': 'age,gender,emotion,accessories'
         }
         data = {'url': img_url}
         response = requests.post(face_api_url, params=params, headers=headers, json=data)
         faces = response.json()
         for face in faces:
-            self.seen.append(face['faceId'])
+            self.seen.append(self.get_attribute(face, 'faceId'))
+
         return faces
 
-    def recognizer(self, id1):
-        """Returns json file of maxNumOdCandidatesReturned faces that match id1, each with confidence level"""
+    def recognizer(self, id1, remove=False):
+        """Returns json file of maxNumOdCandidatesReturned faces that match id1, each with confidence level
+            Deletes id1 and matched face from self.seen"""
         if not isinstance(id1, str):
             raise Exception('Error: id parameter must be of type string')
         face_api_url = 'https://westus.api.cognitive.microsoft.com/face/v1.0/findsimilars'
@@ -40,6 +41,11 @@ class System:
         }
         response = requests.post(face_api_url, json=data, headers=headers)
         recognized = response.json()
+        if remove:
+            self.remove_id(id1)
+            for face in recognized:
+                self.remove_id(self.get_attribute(face, 'faceId'))
+
         return recognized
 
     def get_ids(self):
@@ -48,10 +54,15 @@ class System:
     def remove_id(self, id):
         self.seen.remove(id)
 
+    def get_attribute(self, data, attr):
+        return data[attr]
+
+
+
 
 def test():
     sys = System("553c3c0a400a4f6ea90223e6ae996ce3")
     sys.detect('https://how-old.net/Images/faces2/main007.jpg')
-    sys.recognizer(sys.seen[0])
+    sys.recognizer(sys.seen[0], False)
 
 test()
