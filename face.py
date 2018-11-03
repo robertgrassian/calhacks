5f9ee2080723d08f8a1ecd09fdec5e33de0647fc
Robert Grassian <rgrassian@berkeley.edu>
2018-11-02 23:41:34 -0700

    Cleaned up detecter

--- face.py
+++ face.py
@@ -1,51 +1,28 @@
 import requests
-# If you are using a Jupyter notebook, uncomment the following line.
-#%matplotlib inline
 import matplotlib.pyplot as plt
 from PIL import Image
 from matplotlib import patches
 from io import BytesIO
-
-# Replace <Subscription Key> with your valid subscription key.
 subscription_key = "553c3c0a400a4f6ea90223e6ae996ce3"
 assert subscription_key
 
 
-# You must use the same region in your REST call as you used to get your
-# subscription keys. For example, if you got your subscription keys from
-# westus, replace "westcentralus" in the URI below with "westus".
-#
-# Free trial subscription keys are generated in the westcentralus region.
-# If you use a free trial subscription key, you shouldn't need to change
-# this region.
-face_api_url = 'https://westus.api.cognitive.microsoft.com/face/v1.0/detect'
+def detect(img_url):
 
-# Set image_url to the URL of an image that you want to analyze.
-image_url = 'https://how-old.net/Images/faces2/main007.jpg'
+    face_api_url = 'https://westus.api.cognitive.microsoft.com/face/v1.0/detect'
 
-headers = {'Ocp-Apim-Subscription-Key': subscription_key}
-params = {
-    'returnFaceId': 'true',
-    'returnFaceLandmarks': 'false',
-    'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,' +
-    'emotion,hair,makeup,occlusion,accessories,blur,exposure,noise'
-}
-data = {'url': image_url}
-response = requests.post(face_api_url, params=params, headers=headers, json=data)
-faces = response.json()
+    headers = {'Ocp-Apim-Subscription-Key': subscription_key}
+    params = {
+        'returnFaceId': 'true',
+        'returnFaceLandmarks': 'false',
+        'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,' +
+        'emotion,hair,makeup,occlusion,accessories,blur,exposure,noise'
+    }
+    data = {'url': img_url}
+    response = requests.post(face_api_url, params=params, headers=headers, json=data)
+    faces = response.json()
 
 
-# Display the original image and overlay it with the face information.
-image = Image.open(BytesIO(requests.get(image_url).content))
-plt.figure(figsize=(8, 8))
-ax = plt.imshow(image, alpha=0.6)
-for face in faces:
-    fr = face["faceRectangle"]
-    fa = face["faceAttributes"]
-    origin = (fr["left"], fr["top"])
-    p = patches.Rectangle(
-        origin, fr["width"], fr["height"], fill=False, linewidth=2, color='b')
-    ax.axes.add_patch(p)
-    plt.text(origin[0], origin[1], "%s, %d"%(fa["gender"].capitalize(), fa["age"]),
-             fontsize=20, weight="bold", va="bottom")
-_ = plt.axis("off")
+
+
+detect('https://how-old.net/Images/faces2/main007.jpg')
