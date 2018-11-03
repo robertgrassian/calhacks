2224bc7223fa33ba3e422f2ac6d0982172bd4e8f
Yoneo Arai <yoneo@berkeley.edu>
2018-11-03 13:15:52 -0700

    Finished method to parse JSON, almost done with inserting to SQL.

---
+++ mysql/face_parse_and_store.py
@@ -0,0 +1,19 @@
+import pandas as import pd
+import MySQLdb
+from mysql.constants import *
+from mysql.connection import *
+
+# Takes a pandas dataframe and stores it in the SQL database. 
+def store_to_db(dataframe):
+    # Uses Pandas to_sql method to write to our database. Makes our job very easy.
+    # Establish connection to our server/ database
+    sqlConn = connection.MySqlConnection()
+    
+    # Try to add row to table, otherwise creates the table
+    # TODO Change the table name to the final one that we'll be using
+    try:
+        pd.sql.to_sql(name='testtable', con=sqlConn.connection, if_exists='append')
+    except ValueError as v:
+        print("Value error, this shouldnt happen: " + v)
+
+
