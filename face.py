ca9bb3144abc179e01b61d6ed59ed5ca5abd8c6e
Robert Grassian <rgrassian@berkeley.edu>
2018-11-03 11:29:12 -0700

    added support for local files

--- face.py
+++ face.py
@@ -1,5 +1,5 @@
 import requests
-
+import json
 
 class System:
     def __init__(self, sub_key):
@@ -7,23 +7,30 @@ class System:
         assert self.subscription_key
         self.seen = set()
 
-    def detect(self, img_url):
-        if not isinstance(img_url, str):
+    def detect(self, img, file_type='url'):
+        """Detects faces in img, img must be of type url or binary_data"""
+        if not isinstance(img, str):
             raise Exception('Error: img_url parameter must be of type string')
-        face_api_url = 'https://westus.api.cognitive.microsoft.com/face/v1.0/detect'
 
-        headers = {'Ocp-Apim-Subscription-Key': self.subscription_key}
+        face_api_url = 'https://westus.api.cognitive.microsoft.com/face/v1.0/detect'
         params = {
             'returnFaceId': 'true',
             'returnFaceLandmarks': 'false',
             'returnFaceAttributes': 'age,gender,emotion,accessories'
         }
-        data = {'url': img_url}
-        response = requests.post(face_api_url, params=params, headers=headers, json=data)
+        if file_type == "binary_data":
+            headers = {'Ocp-Apim-Subscription-Key': self.subscription_key,
+                       'Content-Type': 'application/octet-stream'}
+            f = open(img, "rb").read()
+            response = requests.post(face_api_url, params=params, headers=headers, data=f)
+        else:
+            headers = {'Ocp-Apim-Subscription-Key': self.subscription_key}
+            data = {'url': img}
+            response = requests.post(face_api_url, params=params, headers=headers, json=data)
+
         faces = response.json()
         for face in faces:
             self.add_id(face['faceId'])
-
         return faces
 
     def recognizer(self, id, remove=False):
@@ -63,7 +70,9 @@ class System:
 
 def test():
     sys = System("553c3c0a400a4f6ea90223e6ae996ce3")
-    sys.detect('https://how-old.net/Images/faces2/main007.jpg')
+    sys.detect('orl_faces/s1/1.jpg', 'binary_data')
+    # sys.detect('https://how-old.net/Images/faces2/main007.jpg')
+
 
 
 test()
