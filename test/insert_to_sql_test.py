a2c2de075bc0519df9bf154975531cb59f06658a
Yoneo Arai <yoneo@berkeley.edu>
2018-11-03 16:28:00 -0700

    Changed json format

--- test/insert_to_sql_test.py
+++ test/insert_to_sql_test.py
@@ -3,34 +3,34 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from mysql.connection import *
 from parsing.face_json_parse import *
 from mysql.face_parse_and_store import *
-
+from datetime import datetime
 # Instantiate example JSON file
 testJson = {  
-      ‘faceId’:‘a5454d67-b57e-4bd0-bef4-fd3970aacc47’,
-      ‘faceRectangle’:{  
-         ‘top’:29,
-         ‘left’:4,
-         ‘width’:79,
-         ‘height’:79
+      'faceId':"a5454d67-b57e-4bd0-bef4-fd3970aacc47",
+      "faceRectangle":{  
+         "top":29,
+         "left":4,
+         "width":79,
+         "height":79
       },
-      ‘faceAttributes’:{  
-         ‘gender’:‘male’,
-         ‘age’:27.0,
-         ‘emotion’:{  
-            ‘anger’:0.0,
-            ‘contempt’:0.0,
-            ‘disgust’:0.0,
-            ‘fear’:0.0,
-            ‘happiness’:0.0,
-            ‘neutral’:0.975,
-            ‘sadness’:0.025,
-            ‘surprise’:0.0
+      "faceAttributes":{  
+         "gender":"male",
+         "age":27.0,
+         "emotion":{  
+            "anger":0.0,
+            "contempt":0.0,
+            "disgust":0.0,
+            "fear":0.0,
+            "happiness":0.0,
+            "neutral":0.975,
+            "sadness":0.025,
+            "surprise":0.0
          },
-         ‘accessories’:[  
+         "accessories":[  
    
          ]
       },
-      ‘time’:datetime.datetime(2018,
+      "time":datetime.datetime(2018,
       11,
       3,
       13,
