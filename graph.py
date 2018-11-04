e5e6348727daf3b7ad3f7aa44efed88a041c5dc1
Trevor Wise <trevorwise@airbears2-10-142-72-111.airbears2.1918.berkeley.edu>
2018-11-04 01:58:44 -0800

    cleaned up graph

--- graph.py
+++ graph.py
@@ -10,43 +10,11 @@ import json
 plotly.tools.set_credentials_file(username='trevorwise16', api_key='EhhKIRbvdFkt3pfcIBP4')
 
 def graph(arrived, left, total):
-    print("arrived")
-    print(arrived)
-    print("left")
-    print(left)
-    print("total")
-    print(total)
-
-    age = [j['faceAttributes']['age'] for j in total.values()]
     fig = tools.make_subplots(rows=1, cols=2)
-    print(age)
-    #age histogram
-    g1 = go.Histogram(x=age)
 
-
-    labels = ['Male', 'Female']
-    colors = ['#5064ff', '#E1396C']
-
-    men = 0
-    women = 0
-    #TODO: MAybe change just to people in the room
-    for face in total.values():
-        if face['faceAttributes']['gender'] == 'male':
-            men += 1
-        else:
-            women += 1
-    values = [men, women]
-    g2 = go.Pie(labels=labels, values=values,
-                   hoverinfo='label+percent', textinfo='value',
-                   textfont=dict(size=20),
-                   marker=dict(colors=colors,
-                   line=dict(color='#000000', width=2)),
-                domain={'x': [0.0, 0.5], 'y': [0.0, 0.5]})
-
-    timeInClub = [(faceLeft["time"] - total[faceLeft['faceId']]['time']).seconds // 60 for faceLeft in left.values()]
-
-
-    g3 = go.Histogram(x=timeInClub)
+    g1 = graph1(total)
+    g2 = graph2(total)
+    g3 = graph3(total)
 
     fig.append_trace(g1, 1, 1)
     # fig.append_trace(g2, 1, 2)
@@ -67,6 +35,32 @@ def graph(arrived, left, total):
             dif = faceLeft['emotion'].values()[i] - first[i]
 
 '''
+def graph1(total):
+    age = [j['faceAttributes']['age'] for j in total.values()]
+    # age histogram
+    return go.Histogram(x=age)
 
+def graph2(total):
+    labels = ['Male', 'Female']
+    colors = ['#5064ff', '#E1396C']
 
+    men = 0
+    women = 0
+    # TODO: MAybe change just to people in the room
+    for face in total.values():
+        if face['faceAttributes']['gender'] == 'male':
+            men += 1
+        else:
+            women += 1
+    values = [men, women]
+    return go.Pie(labels=labels, values=values,
+                hoverinfo='label+percent', textinfo='value',
+                textfont=dict(size=20),
+                marker=dict(colors=colors,
+                            line=dict(color='#000000', width=2)),
+                domain={'x': [0.0, 0.5], 'y': [0.0, 0.5]})
+
+def graph3(total, left):
+    timeInClub = [(faceLeft["time"] - total[faceLeft['faceId']]['time']).seconds // 60 for faceLeft in left.values()]
+    return go.Histogram(x=timeInClub)
 
