0ef61fa63ed92366783a60255bd3978eaad74c5b
Trevor Wise <trevorwise@airbears2-10-142-72-111.airbears2.1918.berkeley.edu>
2018-11-04 03:05:10 -0800

    more

--- mysql/ssh_tunnel.py
+++ mysql/ssh_tunnel.py
@@ -1,13 +1,12 @@
 from sshtunnel import SSHTunnelForwarder
 import pandas as pd
 from sqlalchemy import create_engine
-
 def ssh_read_in_db():
     with SSHTunnelForwarder(
         ("18.223.212.152", 22),
         ssh_username='ec2-user',
-        ssh_private_key='~/.ssh/calhacks.pem',
-        remote_bind_address=('127.0.0.1', 3306),
+        ssh_private_key='~/Desktop/calhacks.pem',
+        remote_bind_address=('localhost', 3306),
     ) as server:
         conn = create_engine("mysql+mysqldb://server:calhacks@localhost:3306/face_base")
     df = pd.read_sql_table(table_name='testEnter', con=conn)
