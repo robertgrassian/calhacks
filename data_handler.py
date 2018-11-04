4122726441cf0b146023e7db7e2e50b71d5fd38f
Yoneo Arai <yoneo@berkeley.edu>
2018-11-03 21:21:54 -0700

    Checking for duplicates

--- data_handler.py
+++ data_handler.py
@@ -7,11 +7,17 @@ from mysql.read_sql_table import *
 def data_in(jsonIn):
     # Creates df of newly imported data from Face API
     new_df = to_normalized_dataframe(jsonIn)
+    old_sql = read_sql_table("testEnter").drop(['index'], axis=1)['faceId'].unique()
 
+    # Check if ID already in database. If it is, 
+    for id in new_df['faceId'].unique():
+        if id in old_sql:
+            new_df = new_df[new_df['faceId'] != id]
+
+    
     # Appends new df to MySQL db
     store_to_db(new_df, True)
 
     # Creates df based off of current SQL table TODO: Make sure table name is current
     current_data = read_sql_table("testEnter").drop(['index'], axis=1)
-    print(current_data)
     return current_data
