f3721471cac4096c394e9652c5360a2501876910
Yoneo Arai <yoneo@berkeley.edu>
2018-11-03 21:26:20 -0700

    Added error catch to data_handler.py

--- data_handler.py
+++ data_handler.py
@@ -7,16 +7,20 @@ from mysql.read_sql_table import *
 def data_in(jsonIn):
     # Creates df of newly imported data from Face API
     new_df = to_normalized_dataframe(jsonIn)
-    old_sql = read_sql_table("testEnter").drop(['index'], axis=1)['faceId'].unique()
+    # Catch if table doesn't exist
+    try:
+        old_sql = read_sql_table("testEnter").drop(['index'], axis=1)['faceId'].unique()
+            # Check if ID already in database. If it is, 
+        for id in new_df['faceId'].unique():
+            if id in old_sql:
+                new_df = new_df[new_df['faceId'] != id]
 
-    # Check if ID already in database. If it is, 
-    for id in new_df['faceId'].unique():
-        if id in old_sql:
-            new_df = new_df[new_df['faceId'] != id]
-
-    
-    # Appends new df to MySQL db
-    store_to_db(new_df, True)
+        # Appends new df to MySQL db
+        store_to_db(new_df, True)
+    except ValueError as e:
+        print("Table doesn't exist, generating...")
+        store_to_db(new_df, True)
+        
 
     # Creates df based off of current SQL table TODO: Make sure table name is current
     current_data = read_sql_table("testEnter").drop(['index'], axis=1)
